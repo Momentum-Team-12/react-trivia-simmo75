1c489a15c902efb3dc2b4b2cbd8ed7fe44fcc9b4
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-05-27 14:57:02 -0700

    answers-now-show-on-page-ternaries-are-magical

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import "./components/Categories.css";
 import "./App.css";
-import { useState } from "react";
-import { Categories } from "./components/Categories";
+import "./Components/Categories.css";
+import Categories from "./Components/Categories";
 
 function App() {
   return (
     <div className="text">
       <h1>Big Lou's Trivia</h1>
       <h2>Let's Play!</h2>
-      <h3>Pick a category:</h3>
 
       <Categories />
     </div>
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,38 +1,70 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import "./Categories.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export function Categories() {
+function Categories() {
   const [catObjects, setCatObjects] = useState([]);
-  // const [catObject, setSelectedCat] = useState(null);
+  const [questions, setQuestions] = useState([]);
+  const [categorySelected, setCategorySelected] = useState(false);
 
   useEffect(() => {
     axios.get(`https://opentdb.com/api_category.php`).then((res) => {
-      console.log(res.data.trivia_categories);
+      console.log(res.data);
       setCatObjects(res.data.trivia_categories);
     });
   }, []);
 
-  const tempLink = (catId) => {
-    console.log(catId);
+  const handleSetSelectedCat = (catObject) => {
+    axios
+      .get(`https://opentdb.com/api.php?amount=10&category=${catObject.id}`)
+      .then((res) => {
+        console.log(catObject.id);
+        console.log(res.data);
+        setQuestions(res.data.results);
+        setCategorySelected(true);
+      });
   };
 
   return (
-    <div>
-      <div className="container">
-        {catObjects.map((catObject, index) => {
-          const catId = catObject.id;
-          const catName = catObject.name;
-
-          return (
-            <div key={index}>
-              <button className="catButt" onClick={() => tempLink(catId)}>
-                {catName}
-              </button>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      {!categorySelected ? (
+        <>
+          <div>
+            <h2>Pick a Category!</h2>
+            {catObjects.map((catObject, index) => {
+              return (
+                <div key={index}>
+                  <button onClick={() => handleSetSelectedCat(catObject)}>
+                    {catObject.name}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <p>
+              {questions &&
+                questions.map((questions, index) => {
+                  return (
+                    <div key={index}>
+                      <p>{questions.question}</p>
+                      <h3>Pick an Answer!</h3>
+                      <button>{questions.correct_answer}</button>
+                      {questions.incorrect_answers.map((answer, index) => {
+                        return <button key={index}>{answer}</button>;
+                      })}
+                    </div>
+                  );
+                })}
+            </p>
+          </div>
+        </>
+      )}
+    </>
   );
 }
+
+export default Categories;
